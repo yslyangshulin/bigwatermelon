27e1d75803536e01cb87c6d3c95d027e2f53cb55
sssooonnnggg <sssooonnnggg111@gmail.com>
2021-02-01 22:09:53 +0800

    no message

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ window.boot = function() {
             var percent = 100 * completedCount / totalCount;
             if (loadingBool && loadingNum >= 1 && totalCount > 1) {
                 if (percent.toFixed(0) >= 100) {
-                    loadintT.innerHTML = 'loading......100' + '%';
+                    loadintT.innerHTML = '苍苍努力中......100' + '%';
                     setTimeout(function() {
                         loadingBool = false;
                         loadintT.remove();
